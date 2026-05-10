28954de8ca9fe7cd3d900581e78bab31cb94edea
Jaime Jimenez <00081622@uca.edu.sv>
2026-05-09 20:56:38 -0600

    Labo02

---
+++ Laboratorio2/src/main/java/com/example/laboratorio2/domain/entity/Pirate.java
@@ -0,0 +1,34 @@
+package com.example.laboratorio2.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+
+@Entity
+@Table(name = "Pirate")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Pirate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "pirateName")
+    private String name;
+
+    @Column(name = "bounty")
+    private Double bounty;
+
+    @Column(name = "isAlive")
+    private Boolean isAlive;
+
+    @Column(name = "crew")
+    private String crew;
+}
